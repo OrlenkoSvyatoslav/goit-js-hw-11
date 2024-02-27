8469343274750d48693bbf3461599df95bed950a
OrlenkoSvyatoslav <svyat19793@gmail.com>
2024-02-27 22:24:38 +0200

    hw done

--- src/main.js
+++ src/main.js
@@ -3,26 +3,26 @@ import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { getImg } from './js/pixabay-api';
-// import { createMarkup } from './js/render-functions';
+import { createMarkup } from './js/render-functions';
 
-// export const gallery = document.querySelector('.gallery');
+export const gallery = document.querySelector('.gallery');
 const form = document.querySelector('.form');
 export const inputValue = document.querySelector('.input-value-js');
-export const loader = document.querySelector('form-container div');
+export const loader = document.querySelector('.form-container div');
 
 form.addEventListener('submit', onSubmitForm);
 
 function onSubmitForm(event) {
   event.preventDefault();
   gallery.innerHTML = '';
-  if (inputValue.ariaValueMax.trim() === '') {
+  if (inputValue.value.trim() === '') {
     return;
   }
   loader.classList.add('loader');
 
   getImg().then(data => {
     if (data.total === 0) {
-      iziToast.erorr({
+      iziToast.error({
         title: 'Error',
         message:
           'Sorry, there are no images matching your search query. Please try again!',
